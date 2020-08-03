1ba6b6b22be08b5aa8f1ba49f56041e18a740784
Arthur Hjorth <arthur@phys.au.dk>
2020-08-03 11:12:56 +0200

    fixes #1

--- models.py
+++ models.py
@@ -7,6 +7,12 @@ class Activity(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(25))
     open_until = db.Column(Date)
+    data_points = db.relationship('DataPoint', 'activity_data_points')
+
+class ActivityDataPoints(db.Model):
+    __tablename__ = 'activity_data_points'
+    activity_id = db.Column(db.Integer(), db.ForeignKey('activity.id', ondelete='CASCADE'))
+    datapoint_id = db.Column(db.Integer(), db.ForeignKey('data_point.id', ondelete='CASCADE'))
 
 
 class User(db.Model, UserMixin):
@@ -14,15 +20,14 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
-    organizations = db.relationship('Organization', secondary='user_orgs')
     admin = db.Column(db.Boolean, default=False)
     roles = db.relationship('Role', secondary='user_roles')
-    data_points 
+    # data_points  = AH: create a table for this?
 
 
     @staticmethod
     def verify_reset_token(token):
-        s = Serializer(app.config['SECRET_KEY'])
+        s = Serializer(app.config['SECRET_KEY']
         try:
             user_id = s.loads(token)['user_id']
         except:
@@ -36,3 +41,4 @@ class User(db.Model, UserMixin):
 
 class DataPoint(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    activity = db.relationship('Activity', 'activity_data_points')
